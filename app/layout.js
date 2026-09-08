261dd375f84465b2c46c1370bd614c09cf0b5b49
paksh-shadow <shadowpaksh@gmail.com>
2026-09-08 14:49:16 +0530

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -10,27 +10,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
-        {/* Google Search Console Verification */}
-        <meta
-          name="google-site-verification"
-          content="n5mvpFVWwTDSVwOa1opgQ9f6yqtp4sCKYbhmUweGgpQ"
-        />
+        <meta name="google-site-verification" content="n5mvpFVWwTDSVwOa1opgQ9f6yqtp4sCKYbhmUweGgpQ" />
 
         {/* Google AdSense */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9687541571045033"
           crossOrigin="anonymous"
-        />
-
-        {/* Google Analytics */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-Y9XB5WXDL2"
           strategy="afterInteractive"
         />
 
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y9XB5WXDL2" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -39,9 +29,7 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-Y9XB5WXDL2');
           `}
         </Script>
-
       </head>
-
       <body>
         {children}
         <Analytics />
